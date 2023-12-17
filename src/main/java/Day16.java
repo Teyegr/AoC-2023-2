dc35a34a6e80825d571bcf8b410a07e694d98ebb
Fumano <marcdecker2004@gmail.com>
2023-12-17 17:42:06 +0100

    day 16 no recursion

--- src/main/java/Day16.java
+++ src/main/java/Day16.java
@@ -1,6 +1,7 @@
 import util.Day;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Day16 extends Day {
     protected Day16() {
@@ -12,9 +13,8 @@ public class Day16 extends Day {
     @Override
     public long solvePart1(String[] input) {
         char[][] grid = parseInput(input);
-        HashMap<Direction, boolean[][]> visit = depthFirstSearch(grid);
+        HashMap<Direction, boolean[][]> visit = depthFirstSearchStack(grid);
         boolean[][] visited = translate(visit);
-        //visualize(visited);
         return countEnergy(visited);
     }
 
@@ -42,11 +42,10 @@ public class Day16 extends Day {
     }
     private HashMap<Direction, boolean[][]> depthFirstSearch(char[][] grid) {
         HashMap<Direction, boolean[][]> visit = newMap(grid.length, grid[0].length);
-        depthFirstSearch2(0, 0, Direction.RIGHT, grid, visit);
+        depthFirstSearch(0, 0, Direction.RIGHT, grid, visit);
         return visit;
     }
-
-    private void depthFirstSearch2(int i, int j, Direction direction, char[][] grid, HashMap<Direction, boolean[][]> visit) {
+    private void depthFirstSearch(int i, int j, Direction direction, char[][] grid, HashMap<Direction, boolean[][]> visit) {
         while (!(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) && !(visit.get(direction)[i][j])) {
             visit.get(direction)[i][j] = true;
 
@@ -70,7 +69,7 @@ public class Day16 extends Day {
                         case DOWN:
                             i++;break;
                         default:
-                            depthFirstSearch2(i - 1, j, Direction.UP, grid, visit);
+                            depthFirstSearch(i - 1, j, Direction.UP, grid, visit);
                             i++;
                             direction = Direction.DOWN;
                             break;
@@ -83,7 +82,7 @@ public class Day16 extends Day {
                         case LEFT:
                             j--;break;
                         default:
-                            depthFirstSearch2(i, j - 1, Direction.LEFT, grid, visit);
+                            depthFirstSearch(i, j - 1, Direction.LEFT, grid, visit);
                             j++;
                             direction = Direction.RIGHT;
                             break;
@@ -132,6 +131,111 @@ public class Day16 extends Day {
         }
     }
 
+    private HashMap<Direction, boolean[][]> depthFirstSearchStack(char[][] grid) {
+        return depthFirstSearchStack(new Laser(0, 0, Direction.RIGHT), grid, newMap(grid.length, grid[0].length));
+    }
+    private HashMap<Direction, boolean[][]> depthFirstSearchStack(Laser laser, char[][] grid, HashMap<Direction, boolean[][]> visited) {
+        Stack<Laser> laserStack = new Stack<>();
+        laserStack.add(laser);
+        while (!laserStack.isEmpty()) {
+            laser = laserStack.pop();
+            if (laser.i < 0 || laser.j < 0 || laser.i >= grid.length || laser.j >= grid[0].length) {
+                continue;
+            }
+
+            if (visited.get(laser.direction)[laser.i][laser.j]) {
+                continue;
+            }
+
+            visited.get(laser.direction)[laser.i][laser.j] = true;
+
+            int i = laser.i;
+            int j = laser.j;
+            Direction direction = laser.direction;
+            switch (grid[i][j]) {
+                case '.':
+                    switch (direction) {
+                        case UP:
+                            i--;break;
+                        case DOWN:
+                            i++;break;
+                        case RIGHT:
+                            j++;break;
+                        case LEFT:
+                            j--;break;
+                    }
+                    break;
+                case '|':
+                    switch (direction) {
+                        case UP:
+                            i--;break;
+                        case DOWN:
+                            i++;break;
+                        default:
+                            laserStack.add(new Laser(i - 1, j, Direction.UP));
+                            i++;
+                            direction = Direction.DOWN;
+                            break;
+                    }
+                    break;
+                case '-':
+                    switch (direction) {
+                        case RIGHT:
+                            j++;break;
+                        case LEFT:
+                            j--;break;
+                        default:
+                            laserStack.add(new Laser(i, j - 1, Direction.LEFT));
+                            j++;
+                            direction = Direction.RIGHT;
+                            break;
+                    }
+                    break;
+                case '/':
+                    switch (direction) {
+                        case UP:
+                            j++;
+                            direction = Direction.RIGHT;
+                            break;
+                        case DOWN:
+                            j--;
+                            direction = Direction.LEFT;
+                            break;
+                        case RIGHT:
+                            i--;
+                            direction = Direction.UP;
+                            break;
+                        case LEFT:
+                            i++;
+                            direction = Direction.DOWN;
+                            break;
+                    }
+                    break;
+                case '\\':
+                    switch (direction) {
+                        case UP:
+                            j--;
+                            direction = Direction.LEFT;
+                            break;
+                        case DOWN:
+                            j++;
+                            direction = Direction.RIGHT;
+                            break;
+                        case RIGHT:
+                            i++;
+                            direction = Direction.DOWN;
+                            break;
+                        case LEFT:
+                            i--;
+                            direction = Direction.UP;
+                            break;
+                    }
+            }
+            laserStack.add(new Laser(i, j , direction));
+        }
+        return visited;
+    }
+
     private boolean[][] translate(HashMap<Direction, boolean[][]> visit) {
         boolean[][] visited = new boolean[visit.get(Direction.UP).length][visit.get(Direction.UP)[0].length];
         for (int i = 0; i < visit.get(Direction.UP).length; i++) {
@@ -167,7 +271,7 @@ public class Day16 extends Day {
         long maximumEnergy = 0;
         for (int i = 0; i < grid[0].length; i++) {
             HashMap<Direction, boolean[][]> visit = newMap(grid.length, grid[0].length);
-            depthFirstSearch2(0, i, Direction.DOWN, grid, visit);
+            depthFirstSearchStack(new Laser(0, i, Direction.DOWN), grid, visit);
             long energy = countEnergy(translate(visit));
             if (energy > maximumEnergy) {
                 maximumEnergy = energy;
@@ -175,7 +279,7 @@ public class Day16 extends Day {
         }
         for (int i = 0; i < grid[0].length; i++) {
             HashMap<Direction, boolean[][]> visit = newMap(grid.length, grid[0].length);
-            depthFirstSearch2(grid.length - 1, i, Direction.UP, grid, visit);
+            depthFirstSearchStack(new Laser(grid.length - 1, i, Direction.UP), grid, visit);
             long energy = countEnergy(translate(visit));
             if (energy > maximumEnergy) {
                 maximumEnergy = energy;
@@ -183,7 +287,7 @@ public class Day16 extends Day {
         }
         for (int i = 0; i < grid.length; i++) {
             HashMap<Direction, boolean[][]> visit = newMap(grid.length, grid[0].length);
-            depthFirstSearch2(i, 0, Direction.RIGHT, grid, visit);
+            depthFirstSearchStack(new Laser(i, 0, Direction.RIGHT), grid, visit);
             long energy = countEnergy(translate(visit));
             if (energy > maximumEnergy) {
                 maximumEnergy = energy;
@@ -191,7 +295,7 @@ public class Day16 extends Day {
         }
         for (int i = 0; i < grid[0].length; i++) {
             HashMap<Direction, boolean[][]> visit = newMap(grid.length, grid[0].length);
-            depthFirstSearch2(i, grid[0].length - 1, Direction.LEFT, grid, visit);
+            depthFirstSearchStack(new Laser(i, grid[0].length - 1, Direction.LEFT), grid, visit);
             long energy = countEnergy(translate(visit));
             if (energy > maximumEnergy) {
                 maximumEnergy = energy;
@@ -204,5 +308,17 @@ public class Day16 extends Day {
         RIGHT,
         DOWN,
         LEFT
+    }
+
+    static class Laser {
+        int i;
+        int j;
+        Direction direction;
+
+        public Laser(int i, int j, Direction direction) {
+            this.i = i;
+            this.j = j;
+            this.direction = direction;
+        }
     }
 }
---
+++ src/main/java/Day17.java
@@ -0,0 +1,18 @@
+import util.Day;
+
+public class Day17 extends Day {
+    protected Day17() {
+        super(17, true);
+        solvePart1();
+    }
+
+    @Override
+    public long solvePart1(String[] input) {
+        return 0;
+    }
+
+    @Override
+    public long solvePart2(String[] input) {
+        return 0;
+    }
+}
